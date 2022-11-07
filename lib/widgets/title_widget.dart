89e2a7296fcddf4d57cbca68594792fbd6764895
Ihar Kukharchuk <Ihar_Kukharchuk@epam.com>
2022-11-06 19:57:10 -0500

    Add XML parser for FAR

--- lib/widgets/title_widget.dart
+++ lib/widgets/title_widget.dart
@@ -1,14 +1,17 @@
+import 'package:code_of_federal_regulations/code_of_federal_regulations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
-
-import '../screens/title_screen.dart';
 
 class TitleWidget extends StatelessWidget {
   final String id;
   const TitleWidget({super.key, required this.id});
 
   Future<String> _loadFarContent(String id) async {
-    return await rootBundle.loadString('assets/title-14-at-2022-10-17.xml');
+    String cfrContent =
+        await rootBundle.loadString('assets/title-14-at-2022-10-17.xml');
+    CodeOfFederalRegulations cfr =
+        CodeOfFederalRegulations.fromXmlString(cfrContent);
+    return cfr.content.units[0].title;
   }
 
   @override
